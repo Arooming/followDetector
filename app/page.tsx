1b9767b371ae4e75601ec6e176a31487efd7f509
seoAreum <1971236@hansung.ac.kr>
2024-04-04 19:47:59 +0900

    test: 테스트 코드 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+
+import { client } from "@/api";
+import { AxiosResponse } from "axios";
+import { ChangeEvent, useState } from "react";
+
+export interface UserTypes {
+  login?: string;
+  bio?: string;
+  avatar_url?: string;
+}
+
 export default function Home() {
+  const [token, setToken] = useState("");
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+  };
+
+  const handleClickBtn = () => {
+    sessionStorage.setItem("token", token);
+    client()
+      .get<Array<UserTypes>>(`/user/following?per_page=${100}`)
+      .then((res: AxiosResponse<UserTypes[]>) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div>
-      <p>Home</p>
+      <input onChange={handleChangeInput} />
+      <button onClick={handleClickBtn}>Home</button>
     </div>
   );
 }
