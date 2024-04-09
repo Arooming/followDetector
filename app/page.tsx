9badc68d29d56ae9028a49f813e36a9b18da804b
seoAreum <1971236@hansung.ac.kr>
2024-04-10 04:15:31 +0900

    feat: 홈화면 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-
-import { client } from "@/api";
-import { AxiosResponse } from "axios";
+import getFollowInfo from "@/libs/api/getFollowInfo";
+import Link from "next/link";
 import { ChangeEvent, useState } from "react";
+import * as styles from "../style/Home/home.css";
 
 export interface UserTypes {
   login?: string;
@@ -20,17 +20,44 @@ export default function Home() {
 
   const handleClickBtn = () => {
     sessionStorage.setItem("token", token);
-    client()
-      .get<Array<UserTypes>>(`/user/following?per_page=${100}`)
-      .then((res: AxiosResponse<UserTypes[]>) => {
-        console.log(res);
-      });
+    getFollowInfo();
   };
 
   return (
-    <div>
-      <input onChange={handleChangeInput} />
-      <button onClick={handleClickBtn}>Home</button>
-    </div>
+    <main className={styles.HomeWrapper}>
+      <section className={styles.HomeContents}>
+        <div className={styles.TokenLinkBox}>
+          <Link
+            href={"https://github.com/settings/tokens"}
+            className={styles.TokenLinkBtn}
+          >
+            Github Token 만들러 가기
+          </Link>
+          <p className={styles.TokenInfoText}>
+            ﹒ 토큰 발급 시 권한 user(Update ALL user data)를 체크해주세요!
+          </p>
+        </div>
+
+        <article className={styles.TokenInputBox}>
+          <input
+            type="text"
+            placeholder="Github Token을 입력해주세요"
+            onChange={handleChangeInput}
+            className={styles.TokenInput}
+          />
+          
+          <Link href={"/follow-list"}>
+            <button
+              type="button"
+              onClick={handleClickBtn}
+              className={styles.TokenInputNextBtn}
+              disabled={token.length === 0}
+            >
+              나의 맞팔 확인하기
+            </button>
+          </Link>
+        </article>
+      </section>
+    </main>
   );
 }
