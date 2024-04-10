89931365df8302581ad02a22415185f351cc5e97
seoAreum <1971236@hansung.ac.kr>
2024-04-10 20:24:37 +0900

    feat: 사용자의 팔로우 목록을 보여주는 컴포넌트

--- component/FollowList.tsx
+++ component/FollowList.tsx
@@ -1,4 +1,5 @@
 import { FollowInfoDataTypes } from "@/type/user";
+import Image from "next/image";
 
 const FollowList = ({
   followInfoData,
@@ -6,9 +7,50 @@ const FollowList = ({
   followInfoData: FollowInfoDataTypes;
 }) => {
   const { followingData, followersData } = followInfoData;
-  console.log(followingData, followersData);
 
-  return <div>FollowList</div>;
+  const matchedList = followersData.filter((follower) => {
+    return followingData.some(
+      (following) => following.login === follower.login
+    );
+  });
+
+  const unfollowingList = followersData.filter((follower) => {
+    return !matchedList.includes(follower);
+  });
+
+  const user = [matchedList, unfollowingList];
+
+  return (
+    <section>
+      {user.map((list, idx) => {
+        return (
+          <article key={idx}>
+            <p>
+              {list === matchedList
+                ? "맞팔 중인 사용자"
+                : "내가 팔로우 안 한 사용자"}
+            </p>
+            {list.map(({ login, avatar_url, bio }) => {
+              return (
+                <div key={login}>
+                  {avatar_url && (
+                    <Image
+                      width={100}
+                      height={100}
+                      src={avatar_url}
+                      alt={"유저-이미지"}
+                    />
+                  )}
+                  <p>{login}</p>
+                  <p>{bio}</p>
+                </div>
+              );
+            })}
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default FollowList;
