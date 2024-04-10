a43400fad5c4ae3b4554d124e633c704e09a1acd
seoAreum <1971236@hansung.ac.kr>
2024-04-10 19:07:15 +0900

    feat: 불필요한 코드 삭제

--- libs/hook/useGetFollowInfo.ts
+++ libs/hook/useGetFollowInfo.ts
@@ -1,20 +1,10 @@
-import { useRouter } from "next/navigation";
 import { useQuery } from "react-query";
 import getFollowInfo from "../api/getFollowInfo";
 
-const useGetFollowInfo = (token: string | null) => {
-  const router = useRouter();
-
+const useGetFollowInfo = () => {
   const { isLoading, data } = useQuery({
     queryKey: ["get-follow-info"],
     queryFn: () => getFollowInfo(),
-    // 추후 수정 방향
-    // onSuccess: () => token && sessionStorage.setItem("token", token),
-    onError: () => {
-      alert("토큰을 정확히 입력해주세요.");
-      router.push("/");
-      sessionStorage.clear();
-    },
   });
 
   return { isLoading, data };
