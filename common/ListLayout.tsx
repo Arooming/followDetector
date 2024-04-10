98772208d5334a4dba0a38e41047546c0826acb7
seoAreum <1971236@hansung.ac.kr>
2024-04-11 02:21:55 +0900

    feat: 버튼 클릭 시 팔로우/ 언팔로우 기능 구현

--- common/ListLayout.tsx
+++ common/ListLayout.tsx
@@ -1,16 +1,23 @@
-import { UserTypes } from "@/app/page";
+import useDeleteFollower from "@/libs/hook/useDeleteFollower";
+import usePutFollower from "@/libs/hook/usePutFollower";
+import { ListLayoutTypes } from "@/type/user";
 import Image from "next/image";
 import * as styles from "../style/Common/ListLayout.css";
 
-const ListLayout = ({
-  list,
-  isUserInfo,
-  isFollowingBtn,
-}: {
-  list: UserTypes[];
-  isUserInfo: boolean;
-  isFollowingBtn?: boolean;
-}) => {
+const ListLayout = ({ list, isUserInfo, isFollowingBtn }: ListLayoutTypes) => {
+  const putMuation = usePutFollower();
+  const deleteMutation = useDeleteFollower();
+
+  const handleClickFollowBtn = ({
+    isFollowingBtn,
+    login,
+  }: {
+    isFollowingBtn?: boolean;
+    login: string;
+  }) => {
+    isFollowingBtn ? putMuation.mutate(login) : deleteMutation.mutate(login);
+  };
+
   return list.map(({ login, avatar_url }) => {
     return (
       <ul key={login} className={styles.contentsWrapper}>
@@ -20,6 +27,7 @@ const ListLayout = ({
             height={130}
             src={avatar_url}
             alt={"유저-이미지"}
+            priority={true}
             style={{ marginTop: "1rem" }}
           />
         )}
@@ -27,7 +35,13 @@ const ListLayout = ({
           <p className={styles.loginInfo}>{login}</p>
 
           {!isUserInfo && (
-            <button type="button" className={isFollowingBtn ? styles.followBtn : styles.unfollowBtn }>
+            <button
+              type="button"
+              className={isFollowingBtn ? styles.followBtn : styles.unfollowBtn}
+              onClick={() =>
+                login && handleClickFollowBtn({ isFollowingBtn, login })
+              }
+            >
               {isFollowingBtn ? "팔로우" : "언팔로우"}
             </button>
           )}
