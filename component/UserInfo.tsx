1028f7113413738baa9753ee9bdb5bb48867def9
seoAreum <1971236@hansung.ac.kr>
2024-04-10 23:40:43 +0900

    feat: 유저의 정보를 보여주는 컴포넌트

--- component/UserInfo.tsx
+++ component/UserInfo.tsx
@@ -1,8 +1,13 @@
+import ListLayout from "@/common/ListLayout";
 import { UserProfileInfoTypes } from "@/type/user";
+import * as styles from "../style/User/User.css";
 
 const UserInfo = ({ userInfoData }: { userInfoData: UserProfileInfoTypes }) => {
-  console.log(userInfoData);
-  return <div>UserInfo</div>;
+  return (
+    <section className={styles.userInfoWrapper}>
+      <ListLayout list={[userInfoData]} isUserInfo={true} />
+    </section>
+  );
 };
 
 export default UserInfo;
