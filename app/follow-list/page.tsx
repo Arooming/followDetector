10558372ded6a31812a0350001af12df483fe3a1
seoAreum <1971236@hansung.ac.kr>
2024-04-10 19:05:51 +0900

    feat: 컴포넌트 분기처리

--- app/follow-list/page.tsx
+++ app/follow-list/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 
 import Loading from "@/common/Loading";
+import FollowList from "@/component/FollowList";
+import UserInfo from "@/component/UserInfo";
+import getFollowInfo from "@/libs/api/getFollowInfo";
 import useGetFollowInfo from "@/libs/hook/useGetFollowInfo";
+import useGetUserInfo from "@/libs/hook/useGetUserInfo";
+import React from "react";
 
 const page = () => {
-  const token = sessionStorage.getItem("token");
+  const { isLoading: userInfoLoading, data: userInfoData } = useGetUserInfo();
+  const { isLoading: followInfoLoading, data: followInfoData } =
+    useGetFollowInfo();
 
-  const { isLoading, data } = useGetFollowInfo(token);
-
-  return isLoading ? <Loading /> : <div>page</div>;
+  return userInfoLoading || followInfoLoading ? (
+    <Loading />
+  ) : (
+    <React.Fragment>
+      {userInfoData && <UserInfo userInfoData={userInfoData} />}
+      {followInfoData && <FollowList followInfoData={followInfoData} />}
+    </React.Fragment>
+  );
 };
 
 export default page;
