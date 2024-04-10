56d5d668f4ab5807e46efadf4a5341c7878c19ce
seoAreum <1971236@hansung.ac.kr>
2024-04-10 19:07:25 +0900

    feat: 타입 정의

--- type/user.ts
+++ type/user.ts
@@ -1,5 +1,18 @@
+export interface UserProfileInfoTypes {
+  login?: string;
+  bio?: string;
+  avatar_url?: string;
+  followers?: number;
+  following?: number;
+}
+
 export interface UserTypes {
-    login?: string;
-    bio?: string;
-    avatar_url?: string;
-  }
+  login?: string;
+  bio?: string;
+  avatar_url?: string;
+}
+
+export interface FollowInfoDataTypes {
+  followingData: Array<UserTypes>;
+  followersData: Array<UserTypes>;
+}
