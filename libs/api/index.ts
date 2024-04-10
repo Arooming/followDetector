a06d3acf65dd97d02348cb4d0fed3d7286aaac23
seoAreum <1971236@hansung.ac.kr>
2024-04-11 02:22:40 +0900

    chore: 디스크 캐시 관련 주석 추가 및 코드 변경

--- libs/api/index.ts
+++ libs/api/index.ts
@@ -3,6 +3,14 @@ import axios, { AxiosRequestHeaders } from "axios";
 export const client = () => {
   const client = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    headers: {
+      // 디스크 캐시를 방지하기 위한 방법인데 이대로 하면 CORS 에러 발생
+      // "Cache-Control": "no-cache",
+      // Pragma: "no-cache",
+      // Expires: "0",
+
+      Accept: "application/vnd.github+json",
+    },
   });
 
   // interceptors: 요청 전이나 응답 전, 특정 작업 수행
@@ -10,8 +18,6 @@ export const client = () => {
   client.interceptors.request.use(
     (config) => {
       const token = sessionStorage.getItem("token");
-      (config.headers as AxiosRequestHeaders).Accept =
-        "application/vnd.github+json";
       if (token) {
         (
           config.headers as AxiosRequestHeaders
