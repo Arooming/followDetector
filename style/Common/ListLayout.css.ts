c4fc5e143fd8f865d012ab74a5acfc874ba90883
seoAreum <1971236@hansung.ac.kr>
2024-04-26 01:24:41 +0900

    style: 중복 스타일 통합

--- style/Common/ListLayout.css.ts
+++ style/Common/ListLayout.css.ts
@@ -1,43 +1,55 @@
 import { style } from "@vanilla-extract/css";
 
-export const contentsWrapper = style({
+const wrapperBasic = style({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  flexDirection: "column",
-
-  marginBottom: "1rem",
 });
 
-export const followWrapper = style({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  marginTop: "1rem",
+export const contentsWrapper = style([
+  wrapperBasic,
+  {
+    flexDirection: "column",
 
-  gap: "1.2rem",
-});
+    marginBottom: "1rem",
+  },
+]);
+
+export const followWrapper = style([
+  wrapperBasic,
+  {
+    marginTop: "1rem",
+
+    gap: "1.2rem",
+  },
+]);
 
 export const loginInfo = style({
   fontSize: "1.3rem",
 });
 
-export const followBtn = style({
+const followBtnBasic = style({
   padding: "0.3rem 1.3rem",
 
   borderRadius: "3rem",
-  border: "0.1rem solid #FF3333",
-
-  backgroundColor: "#FFE7E7",
-  color: "#FF3333",
 });
 
-export const unfollowBtn = style({
-  padding: "0.3rem 1.3rem",
+export const followBtn = style([
+  followBtnBasic,
+  {
+    border: "0.1rem solid #FF3333",
 
-  borderRadius: "3rem",
-  border: "0.1rem solid #3385FF",
+    backgroundColor: "#FFE7E7",
+    color: "#FF3333",
+  },
+]);
 
-  backgroundColor: "#E7FBFF",
-  color: "#3385FF",
-});
+export const unfollowBtn = style([
+  followBtnBasic,
+  {
+    border: "0.1rem solid #3385FF",
+
+    backgroundColor: "#E7FBFF",
+    color: "#3385FF",
+  },
+]);
