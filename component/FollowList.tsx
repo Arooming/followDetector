8c9bee3f495fb65260c14b4109bcd9e8482f53c1
seoAreum <1971236@hansung.ac.kr>
2024-04-26 02:02:05 +0900

    refactor: useMemo를 활용한 코드로 수정

--- component/FollowList.tsx
+++ component/FollowList.tsx
@@ -1,5 +1,6 @@
 import ListLayout from "@/common/ListLayout";
 import { FollowInfoDataTypes } from "@/type/user";
+import { useMemo } from "react";
 import * as styles from "../style/FollowList/FollowList.css";
 
 const FollowList = ({
@@ -9,33 +10,43 @@ const FollowList = ({
 }) => {
   const { followingData, followersData } = followInfoData;
 
-  const matchedList = followersData.filter((follower) => {
-    return followingData.some(
-      (following) => following.login === follower.login
-    );
-  });
+  // followInfoData가 매우 길어질 수 있는 상황에 대비해 useMemo로 메모이제이션
+  const getUserList = useMemo(() => {
+    const matchedList = followersData.filter((follower) => {
+      return followingData.some(
+        (following) => following.login === follower.login
+      );
+    });
 
-  const unfollowingList = followersData.filter((follower) => {
-    return !matchedList.includes(follower);
-  });
+    const unfollowingList = followersData.filter((follower) => {
+      return !matchedList.includes(follower);
+    });
 
-  const user = [matchedList, unfollowingList];
+    return [{ matched: matchedList }, { unfollowed: unfollowingList }];
+  }, [followersData, followingData]);
 
   return (
     <section className={styles.followListWrapper}>
-      {user.map((list, idx) => {
+      {getUserList.map((list, idx) => {
+        const { matched, unfollowed } = list;
+
         return (
           <article key={idx} className={styles.listWrapper}>
             <div className={styles.titleWrapper}>
               <p className={styles.title}>
-                {list === matchedList
-                  ? "맞팔 중인 사용자"
-                  : "내가 팔로우 안 한 사용자"}
+                {matched ? "맞팔 중인 사용자" : "내가 팔로우 안 한 사용자"}
               </p>
-              <p className={styles.title}>{`${list.length} 명`}</p>
+              <p className={styles.title}>
+                {(matched ? matched.length : unfollowed.length) +
+                  `명`}
+              </p>
             </div>
 
-            <ListLayout list={list} isUserInfo={false} isFollowingBtn={list !== matchedList} />
+            <ListLayout
+              list={matched ? matched : unfollowed}
+              isUserInfo={false}
+              isFollowingBtn={!matched}
+            />
           </article>
         );
       })}
