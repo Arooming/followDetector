ccfe632869eebd3ee0031fc54cb7a6311876a07c
seoAreum <1971236@hansung.ac.kr>
2024-04-29 00:26:56 +0900

    style: link 태그 추가

--- common/Header.tsx
+++ common/Header.tsx
@@ -1,17 +1,21 @@
 import img_github_logo_white from "@/public/image/img_github_logo_white.png";
 import Image from "next/image";
-import * as styles from "../style/Common/Header.css";
+import Link from "next/link";
+import * as styles from "../style/Common/header.css";
 
 const Header = () => {
   return (
-    <header className={styles.HeaderWrapper}>
-      <Image
-        src={img_github_logo_white}
-        alt="깃허브-로고"
-        width={30}
-        height={30}
-      />
-      <h1 className={styles.HeaderTitle}>깃허브 팔로우 탐지기</h1>
+    <header className={styles.headerWrapper}>
+      <Link href={"/"}>
+        <Image
+          src={img_github_logo_white}
+          alt="깃허브-로고"
+          width={30}
+          height={30}
+        />
+      </Link>
+
+      <h1 className={styles.headerTitle}>깃허브 팔로우 탐지기</h1>
     </header>
   );
 };
